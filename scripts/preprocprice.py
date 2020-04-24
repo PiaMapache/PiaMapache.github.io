61208bb5aa2d197447d4270ac2f8cf6854e2e52e
Felipe Mellado Urquiza <famellad@gmail.com>
2020-04-24 19:27:42 -0400

    First working prototype

---
+++ scripts/preprocprice.py
@@ -0,0 +1,12 @@
+pl = open("pricelist.csv")
+print("<table id=\"full-pricelist\">")
+for row in pl:
+    print("  <tr>")
+    cols = row.split(',')
+    for cell in cols:
+        if cols[1] == "\n":
+            print("    <th>" + cell.replace("\n", "") + "</th>")
+        else:
+            print("    <td>" + cell.replace("\n", "") + "</td>")
+    print("  </tr>")
+print("</table>")
